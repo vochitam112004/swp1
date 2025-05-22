00c82f276d53c7264870e0cb54d1fdb6f51849dd
VO CHI TAM <vochitam112004@gmail.com>
2025-05-22 13:41:12 +0700

    new update back-end

---
+++ WebSmokingSpport/Models/CommunityPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebSmokingSpport.Models;
+
+public partial class CommunityPost
+{
+    public int PostId { get; set; }
+
+    public int? MemberId { get; set; }
+
+    public string? Content { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<CommunityInteraction> CommunityInteractions { get; set; } = new List<CommunityInteraction>();
+
+    public virtual MemberProfile? Member { get; set; }
+}
---
+++ WebSmokingSpport/Models/GoalTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebSmokingSpport.Models;
+
+public partial class GoalTemplate
+{
+    public int TemplateId { get; set; }
+
+    public string? Description { get; set; }
+}
