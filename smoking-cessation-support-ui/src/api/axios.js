5d8f6dd6dd8c6d55c59cf2f150b54b08dc302f62
Thanhtu18 <tudtse184093@fpt.edu.vn>
2025-07-04 10:49:26 +0700

    tu

--- smoking-cessation-support-ui/src/api/axios.js
+++ smoking-cessation-support-ui/src/api/axios.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
 
   baseURL: "https://72bb-113-173-227-8.ngrok-free.app/api",// Thay bằng base URL backend của bạn
 
-=======
-  baseURL: "https://29a9-118-69-182-144.ngrok-free.app/api",// Thay bằng base URL backend của bạn
->>>>>>> b746edb46675697473469445944e65916536e9be
 });
 
 // Interceptor để tự gắn token vào header
