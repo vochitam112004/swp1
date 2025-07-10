ce86d6c83281d152052e2c6c70e0673edb32ddae
Loithanhnguyen <lointse182752@fpt.edu.vn>
2025-07-10 21:16:26 +0700

    loi

--- smoking-cessation-support-ui/src/api/axios.js
+++ smoking-cessation-support-ui/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "https://ad314e4efc46.ngrok-free.app/api",// Thay bằng base URL backend của bạn
+  baseURL: "https://4dbe29b63221.ngrok-free.app/api",// Thay bằng base URL backend của bạn
 
 });
 
