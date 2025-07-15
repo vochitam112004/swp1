5418fa78c20ed577e28ce4a180505def4e5edda0
NT1510-L <liemntse184163@fpt.edu.vn>
2025-07-15 22:37:41 +0700

    Update MomoExecuteResponseModel.cs

--- SmokingSupport/WebSmokingSupport/Entity/Momo/MomoExecuteResponseModel.cs
+++ SmokingSupport/WebSmokingSupport/Entity/Momo/MomoExecuteResponseModel.cs
@@ -2,5 +2,10 @@
 {
     public class MomoExecuteResponseModel
     {
+        public string OrderId { get; set; }
+        public string? Amount { get; set; }
+        public string FullName { get; set; }
+        public string OrderInfo { get; set; }
     }
+
 }
