b432699b4ee9fb97678b703554fe0ffb4ae12553
NT1510-L <liemntse184163@fpt.edu.vn>
2025-07-16 12:10:42 +0700

    liem

--- SmokingSupport/WebSmokingSupport/Entity/OrderInfoModel.cs
+++ SmokingSupport/WebSmokingSupport/Entity/OrderInfoModel.cs
@@ -2,5 +2,10 @@
 {
     public class OrderInfoModel
     {
+
+        public string OrderId { get; set; } = string.Empty;
+        public string Amount { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string OrderInfo { get; set; } = string.Empty;
     }
 }
