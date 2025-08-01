c89f34e32abbf30cbcd04174f9f5c41e7319aefd
vochitam112004 <vochitam112004@gmail.com>
2025-08-01 14:02:10 +0700

    Tâm
    đang sửa api GoalPlan

--- SmokingSupport/WebSmokingSupport/Controllers/GoalPlanController.cs
+++ SmokingSupport/WebSmokingSupport/Controllers/GoalPlanController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using WebSmokingSupport.Interfaces;
+using WebSmokingSupport.Entity;
+using WebSmokingSupport.Data;
 namespace WebSmokingSupport.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class GoalPlanController : ControllerBase
     {
+        private readonly IGenericRepository<GoalPlan> _goalPlanRepository;
+        private readonly QuitSmokingSupportContext _context;
+        public GoalPlanController(IGenericRepository<GoalPlan> goalPlanRepository, QuitSmokingSupportContext context)
+        {
+            _goalPlanRepository = goalPlanRepository;
+            _context = context;
+        }
     }
 }
